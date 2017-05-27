1e6ae1ffce667ecde17a3c4191b999088514ecb1
Shams <ekshamsudheen@gmail.com>
2017-05-27 20:05:42 +0530

    public/images directory create feature added

--- src/main/java/com/spring/MusicApplication.java
+++ src/main/java/com/spring/MusicApplication.java
@@ -3,10 +3,19 @@ package com.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 public class  MusicApplication {
 
 	public static void main(String[] args) {
+		File f = new File("public/images");
+		if(!f.isDirectory()){
+			if(f.mkdirs()){
+				System.out.println("public/images directory created!!!");
+			}
+
+		}
 		SpringApplication.run(MusicApplication.class, args);
 	}
 }
